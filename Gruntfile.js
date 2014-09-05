d089cf55daa5f54353762ab40f48094452173820
kim <kim@noerd.dk>
2014-09-05 13:02:04 +0200

    (Added) - init cactus pilot commit

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+/*
+ * grunt-cactuspilot
+ * https://github.com/Solid/grunt-cactuspilot
+ *
+ * Copyright (c) 2013 Kim Frost
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
+
+    // Configuration to be run (and then tested).
+    cactuspilot: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!',
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+    },
+
+	copy: {
+		main: {
+			files: [
+				{ expand: true, cwd: 'css/', src: ['**', '!**/*.db', '!*.db'], dest: 'tempp' },
+			]
+		}
+	},
+
+	compass: {
+		dist: {
+			options: {
+				config: 'config.rb'
+			}
+		}
+	},
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    },
+
+  });
+
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'cactuspilot', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('compile', ['compass']);
+  grunt.registerTask('useme', ['compass']);
+
+};
