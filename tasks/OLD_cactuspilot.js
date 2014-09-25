552f43353fa761001440f9c4cec9f85544644985
Kim Frost <kim@noerd.dk>
2014-09-25 13:33:49 +0200

    (added) - updated it to produce a json data file instead. Intended for angular template instead. Now creates states for both blocks and their elements. No pseudo classes

---
+++ tasks/OLD_cactuspilot.js
@@ -0,0 +1,440 @@
+/*
+ * grunt-cactuspilot
+ * https://github.com/Solid/grunt-cactuspilot
+ *
+ * Copyright (c) 2013 Kim Frost
+ * Licensed under the MIT license.
+ */
+
+
+module.exports = function(grunt) {
+
+	'use strict';
+
+	// Please see the Grunt documentation for more information regarding task
+	// creation: http://gruntjs.com/creating-tasks
+
+
+/*---------------------------------------------------------------
+ DEFAULTS
+---------------------------------------------------------------*/
+	var fs = require('fs'),
+		path = require('path'),
+		plugin = {};
+
+/*---------------------------------------------------------------
+ PLUGIN
+---------------------------------------------------------------*/
+	plugin = {
+		data: {
+			css: null,
+			cactuscss: ""
+		},
+		options: null,
+		getSegmentContent: function(string, type) {
+			var contentList = [];
+
+			var propertySegments = string.split(type+":OPEN");
+
+			for (var i=1;i<propertySegments.length;i++) {
+				var segment = propertySegments[i];
+				var segmentCut = segment.split(type+":CLOSE");
+				var propertyValue = segmentCut[0];
+				//propertyValue = propertyValue.replace(/\/*/g,"").replace(/\*\//g,"").replace(/\*/g,"");
+				propertyValue = propertyValue.replace(/\/\*/g,"").replace(/\*\//g,"").replace(/\*/g,"");
+				propertyValue = propertyValue.trim();
+				contentList.push({
+					type: type,
+					value: propertyValue
+				});
+			}
+			return contentList;
+		},
+		getBlockStates: function(string, blockName) {
+			console.log(" \n --- States Generation --- \n ");
+			var states = [];
+			//console.log("blockName:",blockName);
+			var lineArray = string.toString().split(/\n/);
+
+			for (var i=0;i<lineArray.length;i++) {
+				var line = lineArray[i];
+				if (line.indexOf(blockName) != -1) {
+
+					// Make a raw module without states
+					var state = {
+						name: "",
+						target: blockName,
+						isClass: true
+					};
+					states.push(state); // send the raw module to the states array
+
+					if (line.indexOf("--") != -1) {
+						//console.log("line: ",line);
+
+						var lineString = line;
+						var isClass = false;
+						lineString = lineString.replace(/{/g,"").trim();
+						lineString = lineString.split(" ")[0];
+						if (lineString.indexOf(".") != -1) isClass = true;
+						lineString = lineString.replace(".","").replace(/ /,"");
+						var targetElem = lineString.split("--")[0].replace("--","");
+						var elemState = lineString.split("--")[1].replace("--","");
+
+						console.log("lineString: ",lineString, targetElem, elemState);
+
+						var state = {
+							name: elemState,
+							target: targetElem,
+							isClass: isClass
+						};
+						states.push(state);
+					}
+					else if (line.indexOf(":") != -1) {
+						if (line.indexOf(":hover") != -1 || line.indexOf(":active") != -1) {
+							var lineString = line;
+							var isClass = false;
+							lineString = lineString.replace(/{/g,"").trim();
+							if (lineString.indexOf(".") != -1) isClass = true;
+							lineString = lineString.replace(".","").replace(/ /,"");
+							var targetElem = lineString.split(":")[0];
+							var elemState = lineString.split(":")[1];
+
+							if (elemState === "hover") {
+								elemState = ":hover";
+							}
+							else if (elemState === "active") {
+								elemState = ":active";
+							}
+
+							console.log("lineString: ",lineString, targetElem, elemState);
+
+
+							var state = {
+								name: elemState,
+								target: targetElem,
+								isClass: isClass
+							};
+							states.push(state);
+
+						}
+					}
+				}
+			}
+			console.log(" \n --- !States Generation --- \n ");
+			return states;
+		},
+		getBlockElements: function(string, blockName) {
+			var elements = [];
+			console.log(" \n --- Elements Generation --- \n ");
+			var lineArray = string.toString().split(/\n/);
+			for (var i=0;i<lineArray.length;i++) {
+				var line = lineArray[i];
+				if (line.indexOf(blockName + "__") != -1) {
+
+
+
+				}
+			}
+			console.log(" \n --- !Elements Generation --- \n ");
+			return elements;
+		},
+		enrichWithStates: function(string, list) {
+			// Loop through list and find the items states
+			console.log(" \n --- Enrich Generation --- \n ");
+			for (var i=0;i<list.length;i++) {
+				var elem = list[i];
+				console.log(elem);
+				// Create a states array on the element
+				elem.states = [];
+				// Find all states name elem.value + "--" and append them to elem object under states
+				var lineArray = string.toString().split(/\n/);
+				for (var i=0;i<lineArray.length;i++) {
+					var line = lineArray[i];
+					if (line.indexOf(elem.value + "--") != -1) {
+						console.log(line);
+
+
+
+					}
+				}
+			}
+			console.log(" \n --- !Enrich Generation --- \n ");
+			return list;
+		},
+		generateHtmlPage: function(name,desc,states,markup) {
+			console.log(" \n --- Html Generation --- \n ");
+			var html = "";
+
+			//console.log(name);
+			//console.log(desc);
+			//console.log(states);
+			//console.log(markup);
+
+			html += "<html>";
+			html += "<head>";
+				html += "<title>"+ name +"</title>";
+				// put css src file relation
+				html += '<link href="'+ plugin.data.src +'" rel="stylesheet" type="text/css">';
+				// put cactus css file relation
+				html += '<link href="cactuspilot.css" rel="stylesheet" type="text/css">';
+
+				html += '<script src="cactuspilot.js"></script>';
+
+			html += "</head>";
+			html += "<body>";
+
+				html += "<div class='l-modules'>";
+					html += "<div class='l-modules__navigation'></div>";
+
+					html += "<div class='l-modules__title'>";
+						html += "<h1>"+ name +"</h1>";
+					html += "</div>";
+
+					html += "<div class='l-modules__description'>";
+						html += "<p>"+ desc +"</p>";
+					html += "</div>";
+
+					html += "<div class='l-modules__list'>";
+						var statesGenerated = [];
+						// calculate variants
+						for (var i=0;i<states.length;i++) {
+							var state = states[i];
+
+							// check for dublications and skip them
+							var generate = true;
+							for (var m=0;m<statesGenerated.length;m++) {
+								var generatedState = statesGenerated[m]
+								if (state.name === generatedState.name && state.target === generatedState.target) {
+									generate = false;
+									break;
+								}
+							}
+							if (generate) {
+
+								console.log("   State.name:" + state.name +"   State.target:" + state.target);
+
+								var blockMarkup = markup;
+
+								if (state.name.indexOf(":") != -1) {
+									html += '<p>'+ state.target + state.name +'</p>';
+									blockMarkup = blockMarkup.replace(state.target, state.target + " " + state.name);
+								}
+								else {
+									html += '<p>'+ state.target+"--"+state.name +'</p>';
+									blockMarkup = blockMarkup.replace(state.target, state.target + " " + state.target + "--" + state.name);
+
+								}
+
+								html += "<div class='cactus-module'>";
+
+								html += blockMarkup;
+								html += "</div>";
+
+
+
+
+
+								/*
+								if (state.target === name) {
+									var blockMarkup = markup;
+
+									if (state.name.indexOf(":") != -1) {
+										html += '<p>'+ state.target + state.name +'</p>';
+										blockMarkup = blockMarkup.replace("$$modifiers", state.name);
+									}
+									else {
+										html += '<p>'+ state.target+"--"+state.name +'</p>';
+										blockMarkup = blockMarkup.replace("$$modifiers", state.target+"--"+state.name);
+									}
+
+									html += "<div class='cactus-module'>";
+
+										html += blockMarkup;
+									html += "</div>";
+								}
+								*/
+								statesGenerated.push(state);
+							}
+						}
+					html += "</div>";
+				html += "</div>";
+			html += "</body>";
+			html += "</html>";
+
+			console.log(" \n --- !Html Generation --- \n ");
+			return html;
+		}
+	}
+/*---------------------------------------------------------------
+ TASK
+---------------------------------------------------------------*/
+
+	grunt.registerMultiTask('cactuspilot', 'Sass BEM styleguide generation', function() {
+		// Merge task-specific and/or target-specific options with these defaults.
+
+		//console.log("options -->",this.options);
+
+		var cactuspilot = {
+			options: this.options(),
+			async: grunt.util.async,
+			done: this.async(),
+			files: this.files
+		};
+
+		//grunt.task.run('compile');
+
+		//console.log("-->", cactuspilot);
+
+		//console.log("Find me", this);
+
+		//console.log("--> Files: ",this.files);
+
+		// Iterate over all specified file groups.
+		this.files.forEach(function(f) {
+
+			grunt.log.writeln("--> file", f.dest);
+			grunt.log.writeln("--> file", f.src);
+
+			plugin.data.src = f.src;
+
+			// Create destination folder
+			if (fs.existsSync(f.dest)) {
+				//console.log("folder is there");
+			}
+			else {
+				//console.log("folder is not there");
+				fs.mkdir(f.dest, function(err) {
+					if (err) { throw err; }
+				});
+			}
+
+			// Check if source is there
+			var src = f.src.filter(function(filepath) {
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				}
+				else {
+					return true;
+				}
+			}).map(function(filepath) {
+				// Read file source.
+				return grunt.file.read(filepath);
+			});
+
+			var testArray = src.toString().split(/\n/);
+
+			//grunt.log.writeln("------------Source------------");
+			//grunt.log.writeln("src",src);
+			//grunt.log.writeln(src);
+			//grunt.log.writeln("-----------!Source------------");
+
+
+
+			// Get a list of all modules
+			var modules = src.toString().split("/*CACTUSPILOT:OPEN*/");
+			for (var i=1;i<modules.length;i++) {
+				var segment = modules[i];
+				var segmentCut = segment.split("/*CACTUSPILOT:CLOSE*/");
+				var moduleSegment = segmentCut[0];
+				//grunt.log.writeln("------------Module------------");
+				//grunt.log.writeln(moduleSegment);
+				//grunt.log.writeln("-----------!Module------------");
+
+				var modulename = plugin.getSegmentContent(moduleSegment, "MODULE");
+				var moduleDesc = plugin.getSegmentContent(moduleSegment, "DESC");
+				var moduleMarkup = plugin.getSegmentContent(moduleSegment, "MARKUP");
+
+
+				var modulesElements = plugin.getBlockElements(src, modulename[0].value);
+				modulesElements.push(modulename[0]);
+				var blockAndElements = modulesElements;
+
+				var blockAndElementsEnriched = plugin.enrichWithStates(src, blockAndElements);
+
+
+
+
+
+
+
+
+
+
+
+
+				/*
+				var moduleStates = plugin.getBlockStates(src, modulename[0].value);
+
+				var generatedHtml = plugin.generateHtmlPage(modulename[0].value, moduleDesc[0].value, moduleStates, moduleMarkup[0].value);
+
+				//Create test file for module in destination
+				grunt.file.write(f.dest + modulename[0].value + ".html", generatedHtml, {
+					encoding: "utf-8"
+				});
+
+				// copy cactus style to dest folder
+				*/
+
+
+			}
+
+
+
+
+
+
+			/*
+			var stringArray = src.toString().split("\/*START*\/");
+			for (var i=1;i<stringArray.length;i++) {
+				var segment = stringArray[i];
+				//grunt.log.writeln("segment",segment);
+				var segmentCut = segment.split("\/*END*\/");
+				var moduleSegment = segmentCut[0];
+				grunt.log.writeln("------------Module------------");
+				//grunt.log.writeln(moduleSegment);
+				grunt.log.writeln("-----------!Module------------");
+
+				var modulename = "";
+				var modulenameArray = moduleSegment.split("MODULE:");
+				grunt.log.writeln(modulenameArray[1]);
+
+				//Create test file for module in destination
+				grunt.file.write(f.dest + Math.random() + ".html", "<html><head></head><body><p>MEH!</p><div>"+src+"</div></body></html>", {
+					encoding: "utf-8"
+				});
+			}
+			*/
+
+			/*
+
+			// Concat specified files.
+			var src = f.src.filter(function(filepath) {
+				// Warn on and remove invalid source files (if nonull was set).
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				}
+				else {
+					return true;
+				}
+			}).map(function(filepath) {
+				// Read file source.
+				return grunt.file.read(filepath);
+			}).join(grunt.util.normalizelf(options.separator));
+
+			// Handle options.
+			src += options.punctuation;
+
+			// Write the destination file.
+			grunt.file.write(f.dest, src);
+
+			// Print a success message.
+			grunt.log.writeln('File "' + f.dest + '" created.');
+
+			*/
+
+		});
+	});
+
+};
--- tasks/cactuspilot.js
+++ tasks/cactuspilot.js
@@ -31,16 +31,16 @@ module.exports = function(grunt) {
 			cactuscss: ""
 		},
 		options: null,
+		generateHtmlPage: function() {
+
+		},
 		getSegmentContent: function(string, type) {
 			var contentList = [];
-
 			var propertySegments = string.split(type+":OPEN");
-
-			for (var i=1;i<propertySegments.length;i++) {
+			for (var i=1;i<propertySegments.length;i++	) {
 				var segment = propertySegments[i];
 				var segmentCut = segment.split(type+":CLOSE");
 				var propertyValue = segmentCut[0];
-				//propertyValue = propertyValue.replace(/\/*/g,"").replace(/\*\//g,"").replace(/\*/g,"");
 				propertyValue = propertyValue.replace(/\/\*/g,"").replace(/\*\//g,"").replace(/\*/g,"");
 				propertyValue = propertyValue.trim();
 				contentList.push({
@@ -50,181 +50,70 @@ module.exports = function(grunt) {
 			}
 			return contentList;
 		},
-		getBlockStates: function(string, blockName) {
-			console.log(" \n --- States Generation --- \n ");
-			var states = [];
-			//console.log("blockName:",blockName);
+		// Get Elements of blockname
+		getElements: function(string, blockName) {
+			grunt.log.writeln("------------getElements------------");
+			var elements = [];
+			var elementsEnriched = [];
 			var lineArray = string.toString().split(/\n/);
-
 			for (var i=0;i<lineArray.length;i++) {
 				var line = lineArray[i];
-				if (line.indexOf(blockName) != -1) {
-
-					// Make a raw module without states
-					var state = {
-						name: "",
-						target: blockName,
-						isClass: true
-					};
-					states.push(state); // send the raw module to the states array
-
-					if (line.indexOf("--") != -1) {
-						//console.log("line: ",line);
-
-						var lineString = line;
-						var isClass = false;
-						lineString = lineString.replace(/{/g,"").trim();
-						lineString = lineString.split(" ")[0];
-						if (lineString.indexOf(".") != -1) isClass = true;
-						lineString = lineString.replace(".","").replace(/ /,"");
-						var targetElem = lineString.split("--")[0].replace("--","");
-						var elemState = lineString.split("--")[1].replace("--","");
-
-						console.log("lineString: ",lineString, targetElem, elemState);
-
-						var state = {
-							name: elemState,
-							target: targetElem,
-							isClass: isClass
-						};
-						states.push(state);
-					}
-					else if (line.indexOf(":") != -1) {
-						if (line.indexOf(":hover") != -1 || line.indexOf(":active") != -1) {
-							var lineString = line;
-							var isClass = false;
-							lineString = lineString.replace(/{/g,"").trim();
-							if (lineString.indexOf(".") != -1) isClass = true;
-							lineString = lineString.replace(".","").replace(/ /,"");
-							var targetElem = lineString.split(":")[0];
-							var elemState = lineString.split(":")[1];
-
-							if (elemState === "hover") {
-								elemState = ":hover";
-							}
-							else if (elemState === "active") {
-								elemState = ":active";
-							}
-
-							console.log("lineString: ",lineString, targetElem, elemState);
-
-
-							var state = {
-								name: elemState,
-								target: targetElem,
-								isClass: isClass
-							};
-							states.push(state);
-
+				if (line.indexOf(blockName + "__") != -1) {
+					var temp = line.split(/\{/g)[0];
+					var elementList = temp.split(/\,/g);
+					for (var ii=0;ii<elementList.length;ii++) {
+						var element = elementList[ii];
+						var index = element.indexOf("__");
+						element = element.split('__')[1];
+						element = element.split('--')[0];
+						element = element.replace(/ /g,"");
+						element = blockName + '__' + element;
+						if (elements.indexOf(element) === -1) {
+							elements.push(element);
+							grunt.log.writeln('element: ' + element);
 						}
 					}
 				}
 			}
-			console.log(" \n --- !States Generation --- \n ");
-			return states;
+			grunt.log.writeln("------------!getElements------------");
+			for (var i=0;i<elements.length;i++) {
+				var element = elements[i];
+				elementsEnriched.push({
+					name: element,
+					states: []
+				});
+			}
+			return elementsEnriched;
 		},
-		generateHtmlPage: function(name,desc,states,markup) {
-			console.log(" \n --- Html Generation --- \n ");
-			var html = "";
+		getStates: function(string, name) {
+			grunt.log.writeln("------------getStates------------");
+			grunt.log.writeln('for: ' + name);
+			var states = [];
+			var lineArray = string.toString().split(/\n/);
+			for (var i=0;i<lineArray.length;i++) {
+				var line = lineArray[i];
+				if (line.indexOf(name + "--") != -1) {
+					var temp = line.split(/\{/g)[0];
+					var statesList = temp.split(/\,/g);
+					for (var ii=0;ii<statesList.length;ii++) {
+						var state = statesList[ii];
+						var index = state.indexOf("--");
+						state = state.split('--')[1];
+						state = state.split(' ')[0];
+						state = state.replace(/ /g,"");
 
-			//console.log(name);
-			//console.log(desc);
-			//console.log(states);
-			//console.log(markup);
+						// Remove pseudo classes ?
+						state = state.replace(/\:hover/g,"").replace(/\::hover/g,"").replace(/\:active/g,"").replace(/\::active/g,"");
 
-			html += "<html>";
-			html += "<head>";
-				html += "<title>"+ name +"</title>";
-				// put css src file relation
-				html += '<link href="'+ plugin.data.src +'" rel="stylesheet" type="text/css">';
-				// put cactus css file relation
-				html += '<link href="cactuspilot.css" rel="stylesheet" type="text/css">';
-
-				html += '<script src="cactuspilot.js"></script>';
-
-			html += "</head>";
-			html += "<body>";
-
-				html += "<div class='l-modules'>";
-					html += "<div class='l-modules__navigation'></div>";
-
-					html += "<div class='l-modules__title'>";
-						html += "<h1>"+ name +"</h1>";
-					html += "</div>";
-
-					html += "<div class='l-modules__description'>";
-						html += "<p>"+ desc +"</p>";
-					html += "</div>";
-
-					html += "<div class='l-modules__list'>";
-						var statesGenerated = [];
-						// calculate variants
-						for (var i=0;i<states.length;i++) {
-							var state = states[i];
-
-							// check for dublications and skip them
-							var generate = true;
-							for (var m=0;m<statesGenerated.length;m++) {
-								var generatedState = statesGenerated[m]
-								if (state.name === generatedState.name && state.target === generatedState.target) {
-									generate = false;
-									break;
-								}
-							}
-							if (generate) {
-
-								console.log("   State.name:" + state.name +"   State.target:" + state.target);
-
-								var blockMarkup = markup;
-
-								if (state.name.indexOf(":") != -1) {
-									html += '<p>'+ state.target + state.name +'</p>';
-									blockMarkup = blockMarkup.replace(state.target, state.target + " " + state.name);
-								}
-								else {
-									html += '<p>'+ state.target+"--"+state.name +'</p>';
-									blockMarkup = blockMarkup.replace(state.target, state.target + " " + state.target + "--" + state.name);
-
-								}
-
-								html += "<div class='cactus-module'>";
-
-								html += blockMarkup;
-								html += "</div>";
-
-
-
-
-
-								/*
-								if (state.target === name) {
-									var blockMarkup = markup;
-
-									if (state.name.indexOf(":") != -1) {
-										html += '<p>'+ state.target + state.name +'</p>';
-										blockMarkup = blockMarkup.replace("$$modifiers", state.name);
-									}
-									else {
-										html += '<p>'+ state.target+"--"+state.name +'</p>';
-										blockMarkup = blockMarkup.replace("$$modifiers", state.target+"--"+state.name);
-									}
-
-									html += "<div class='cactus-module'>";
-
-										html += blockMarkup;
-									html += "</div>";
-								}
-								*/
-								statesGenerated.push(state);
-							}
+						if (states.indexOf(state) === -1) {
+							states.push(state);
+							grunt.log.writeln('state: ' + state);
 						}
-					html += "</div>";
-				html += "</div>";
-			html += "</body>";
-			html += "</html>";
-
-			console.log(" \n --- !Html Generation --- \n ");
-			return html;
+					}
+				}
+			}
+			grunt.log.writeln("------------!getStates------------");
+			return states;
 		}
 	}
 /*---------------------------------------------------------------
@@ -232,9 +121,6 @@ module.exports = function(grunt) {
 ---------------------------------------------------------------*/
 
 	grunt.registerMultiTask('cactuspilot', 'Sass BEM styleguide generation', function() {
-		// Merge task-specific and/or target-specific options with these defaults.
-
-		//console.log("options -->",this.options);
 
 		var cactuspilot = {
 			options: this.options(),
@@ -243,22 +129,10 @@ module.exports = function(grunt) {
 			files: this.files
 		};
 
-		//grunt.task.run('compile');
-
-		//console.log("-->", cactuspilot);
-
-		//console.log("Find me", this);
-
-		//console.log("--> Files: ",this.files);
-
-		// Iterate over all specified file groups.
 		this.files.forEach(function(f) {
-
 			grunt.log.writeln("--> file", f.dest);
 			grunt.log.writeln("--> file", f.src);
-
 			plugin.data.src = f.src;
-
 			// Create destination folder
 			if (fs.existsSync(f.dest)) {
 				//console.log("folder is there");
@@ -270,7 +144,8 @@ module.exports = function(grunt) {
 				});
 			}
 
-			// Check if source is there
+			/* Check if source is there
+		 	************************************************************/
 			var src = f.src.filter(function(filepath) {
 				if (!grunt.file.exists(filepath)) {
 					grunt.log.warn('Source file "' + filepath + '" not found.');
@@ -284,113 +159,53 @@ module.exports = function(grunt) {
 				return grunt.file.read(filepath);
 			});
 
-			var testArray = src.toString().split(/\n/);
+			//var testArray = src.toString().split(/\n/); // Split string into lines array
 
-			//grunt.log.writeln("------------Source------------");
-			//grunt.log.writeln("src",src);
-			//grunt.log.writeln(src);
-			//grunt.log.writeln("-----------!Source------------");
-
-
+			/* Get a list of all modules
+			************************************************************/
 			var modules = src.toString().split("/*CACTUSPILOT:OPEN*/");
+			var dataList = [];
+			// For each module
 			for (var i=1;i<modules.length;i++) {
 				var segment = modules[i];
 				var segmentCut = segment.split("/*CACTUSPILOT:CLOSE*/");
-				var moduleSegment = segmentCut[0];
-				//grunt.log.writeln("------------Module------------");
-				//grunt.log.writeln(moduleSegment);
-				//grunt.log.writeln("-----------!Module------------");
+				var cssAfterCactus = segmentCut[1];
+				var cactusSegment = segmentCut[0];
 
-				var modulename = plugin.getSegmentContent(moduleSegment, "MODULE");
-				var moduleDesc = plugin.getSegmentContent(moduleSegment, "DESC");
-				var moduleMarkup = plugin.getSegmentContent(moduleSegment, "MARKUP");
+				var modulename = plugin.getSegmentContent(cactusSegment, "MODULE")[0].value;
+				var moduleDesc = plugin.getSegmentContent(cactusSegment, "DESC")[0].value;
+				var moduleMarkup = plugin.getSegmentContent(cactusSegment, "MARKUP")[0].value;
 
+				var data = {
+					name: modulename,
+					states: plugin.getStates(cssAfterCactus, modulename),
+					elements: plugin.getElements(cssAfterCactus, modulename)
+				}
 
-				var moduleStates = plugin.getBlockStates(src, modulename[0].value);
+				// Enrich elements with states
+				for (var ii=0;ii<data.elements.length;ii++) {
+					var element = data.elements[ii];
+					//data.elements[ii].states = plugin.getStates(cssAfterCactus, element.name);
+					element.states = plugin.getStates(cssAfterCactus, element.name);
+				}
 
-				//console.log(modulename);
-				//console.log(moduleStates);
-				//console.log(moduleDesc);
-				//console.log(moduleMarkup);
+				//var generatedHtml = modulename;
 
 				/*
-				var strippedModuleStates = [];
-				for (var m=0;m<moduleStates.length;m++) {
-					var state = moduleStates[m];
-					var _state = {
-
-					};
-					strippedModuleStates.push(_state);
-				}
+				grunt.file.write(f.dest + modulename + ".html", generatedHtml, {
+					encoding: "utf-8"
+				});
 				*/
 
-				var generatedHtml = plugin.generateHtmlPage(modulename[0].value, moduleDesc[0].value, moduleStates, moduleMarkup[0].value);
-
-				//Create test file for module in destination
-				grunt.file.write(f.dest + modulename[0].value + ".html", generatedHtml, {
-					encoding: "utf-8"
-				});
-
-				// copy cactus style to dest folder
-
-
-
-
+				dataList.push(data);
 			}
 
-
-
-			/*
-			var stringArray = src.toString().split("\/*START*\/");
-			for (var i=1;i<stringArray.length;i++) {
-				var segment = stringArray[i];
-				//grunt.log.writeln("segment",segment);
-				var segmentCut = segment.split("\/*END*\/");
-				var moduleSegment = segmentCut[0];
-				grunt.log.writeln("------------Module------------");
-				//grunt.log.writeln(moduleSegment);
-				grunt.log.writeln("-----------!Module------------");
-
-				var modulename = "";
-				var modulenameArray = moduleSegment.split("MODULE:");
-				grunt.log.writeln(modulenameArray[1]);
-
-				//Create test file for module in destination
-				grunt.file.write(f.dest + Math.random() + ".html", "<html><head></head><body><p>MEH!</p><div>"+src+"</div></body></html>", {
-					encoding: "utf-8"
-				});
-			}
-			*/
-
-			/*
-
-			// Concat specified files.
-			var src = f.src.filter(function(filepath) {
-				// Warn on and remove invalid source files (if nonull was set).
-				if (!grunt.file.exists(filepath)) {
-					grunt.log.warn('Source file "' + filepath + '" not found.');
-					return false;
-				}
-				else {
-					return true;
-				}
-			}).map(function(filepath) {
-				// Read file source.
-				return grunt.file.read(filepath);
-			}).join(grunt.util.normalizelf(options.separator));
-
-			// Handle options.
-			src += options.punctuation;
-
-			// Write the destination file.
-			grunt.file.write(f.dest, src);
-
-			// Print a success message.
-			grunt.log.writeln('File "' + f.dest + '" created.');
-
-			*/
+			// Creta a single data json file
+			var jsonData = JSON.stringify({blocks:dataList});
+			grunt.file.write(f.dest + 'data' + ".json", jsonData, {
+				encoding: "utf-8"
+			});
 
 		});
 	});
-
 };
